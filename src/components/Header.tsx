d764d62b16cd82c8aba006176cefa0f834846df5
Alex Kostinskyi <alex.kostinskyi@gmail.com>
2020-05-01 09:28:39 +0000

    Header.tsx edited online with Bitbucket

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,7 +18,7 @@ function Header({ showTagline }: Props) {
       <Link to="/">
         <img alt="Drop" className="logo mt-5 mb-3" src={require('../images/logo-vector.svg')} width="75" height="75" />
       </Link>
-      {showTagline && <h1>Мобільний додаток веганської їжі та екотоварів від українських виробників</h1>}
+      {showTagline && <h1>Мобільний додаток екотоварів та веганської їжі від українських виробників</h1>}
     </div>
   );
 }
